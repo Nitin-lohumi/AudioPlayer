50fdc18c11eb5f1e599348c9a3df9573a10906aa
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-07-25 21:06:47 +0530

    add

--- src/Component/MusicPlayer.jsx
+++ src/Component/MusicPlayer.jsx
@@ -5,7 +5,9 @@ import { FaAngleDoubleLeft } from "react-icons/fa";
 import { FaAngleDoubleRight } from "react-icons/fa";
 function MusicPlayer(){
   const [click, setClick] = useState(0);
-  const audioRef = useRef();
+  const audioRef = useRef('0');
+  const [playprogress,setPlayprogress] = useState(0);
+  const [duration,setDuration] = useState(audioRef.current.duration);
   useEffect(()=>{
     if(click==1){
       load();
@@ -20,18 +22,29 @@ function MusicPlayer(){
       var remainingSeconds = sec % 60;
       console.log(Math.floor(sec/60)+ ":" + (remainingSeconds).toFixed(0));
     }
-
+    // function playTime(){
+    //   if(click==0){
+    //     console.log(Math.floor(audioRef.current.currentTime));
+    //     setDuration(Math.floor(audioRef.current.currentTime));
+    //   }
+    // }
    function PlayPause(){
+    let intervalId;
     if(click==0){
       audioRef.current.play();
+      intervalId = setInterval(()=>{
+          console.log(Math.floor(audioRef.current.currentTime));
+          setDuration(Math.floor(audioRef.current.currentTime));
+        },1000);
       setClick(1);
     }
     else{
       audioRef.current.pause();
       setClick(0);
+      clearInterval(intervalId);
     }
-    
   }
+  
    return (
     <>
       <div className="music_area">
@@ -40,6 +53,7 @@ function MusicPlayer(){
         <audio controlsList="nodownload noplaybackrate" ref={audioRef}>
           <source src="\Audio\music1.m4a"/>
         </audio>
+      <p>{duration}</p>  : <p>{Math.floor(audioRef.current.duration/60)+ ":" + (audioRef.current.duration%60).toFixed(0)}</p>
         <div className="Controls">
           <button><FaAngleDoubleLeft/></button>
           <button onClick={PlayPause}>{click==0?<FaPlay/>:<FaPause/>}</button>
